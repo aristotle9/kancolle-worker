d9d7153146fe93f1cf3e7b3079243a4f16a4e245
aristotle9 <lanfan.1987@gmail.com>
2014-01-20 18:24:45 +0800

    add id->map state

--- src/kancolle_worker/game.clj
+++ src/kancolle_worker/game.clj
@@ -3,9 +3,21 @@
 
 (def ^:dynamic *state* (atom {}))
 
+(defn id->ship
+  [id]
+  (get-in @*state* [:id->ship id]))
+
 (defn update-ship!
   []
-  (let [ships (api/ship)]
-    (swap! *state* assoc :ships ships)
-    (if ships true false)))
+  (if-let [ships (api/ship)]
+    (let [id->ship (zipmap (map :id ships) ships)]
+      (swap! *state* assoc :ships ships)
+      (swap! *state* assoc :id->ship id->ship)
+      true)))
 
+(defn update-deck-port!
+  []
+  (if-let [deck-port (api/deck-port)]
+    (do
+      (swap! *state* assoc :deck-port deck-port)
+      true)))
